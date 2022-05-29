4476b1c2d42788a035cd6f1e09541e09cf07bcc2
loading <49134864+load1n9@users.noreply.github.com>
2022-05-29 16:25:28 -0400

    feat: generator + example

--- examples/basic.ts
+++ examples/basic.ts
@@ -1,5 +1,10 @@
 
-import { Parser } from '../mod.ts';
+import { Parser, Generator } from '../mod.ts';
 
 const parser = new Parser(Deno.readFileSync("./test.mid"));
 console.log(parser.midi?.track[0].event)
+
+
+const generator = new Generator({tracks: [parser.midi!.track[0]]});
+const midi = generator.write();
+console.log(midi)
--- mod.ts
+++ mod.ts
@@ -1,5 +1,5 @@
 export { toHexString, printFile } from './utils/mod.ts';
 export { Parser } from './src/parser.ts';
-
+export {  Generator } from './src/generator.ts';
 
 
